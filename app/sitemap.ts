6a087625bc7421d2fd705f310221b348f6dde3b8
PatriotStunfisk <c_elegans271828@yahoo.co.jp>
2026-09-12 08:58:07 +0900

    Index purchase-intent fishing guides

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,9 +9,10 @@ import {extraGuideArticles2} from '@/lib/guide-articles-extra2';
 import {extraGuideArticles3} from '@/lib/guide-articles-extra3';
 import {extraGuideArticles4} from '@/lib/guide-articles-extra4';
 import {extraGuideArticles5} from '@/lib/guide-articles-extra5';
+import {extraGuideArticles6} from '@/lib/guide-articles-extra6';
 
 const methodDetails={...baseMethodDetails,...extraMethodDetails,...extraMethodDetails2};
-const allGuides=[...guideArticles,...extraGuideArticles,...extraGuideArticles2,...extraGuideArticles3,...extraGuideArticles4,...extraGuideArticles5];
+const allGuides=[...guideArticles,...extraGuideArticles,...extraGuideArticles2,...extraGuideArticles3,...extraGuideArticles4,...extraGuideArticles5,...extraGuideArticles6];
 
 export default function sitemap():MetadataRoute.Sitemap{
   const base='https://uolink.vercel.app';
