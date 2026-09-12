3e90f93e378171cb97a4524b92869e285db6a145
PatriotStunfisk <c_elegans271828@yahoo.co.jp>
2026-09-12 20:45:58 +0900

    Enable 3D viewer on kasago fish page

--- components/FishVisual.tsx
+++ components/FishVisual.tsx
@@ -6,6 +6,7 @@ import BuriMedia from './BuriMedia';
 import SabaMedia from './SabaMedia';
 import AjiMedia from './AjiMedia';
 import MadaiMedia from './MadaiMedia';
+import KasagoMedia from './KasagoMedia';
 import s from './FishVisual.module.css';
 
 type Props={slug:string;name:string;className?:string};
@@ -63,6 +64,7 @@ export default function FishVisual({slug,name,className=''}:Props){
   if(slug==='saba'&&pathname==='/fish/saba')return <SabaMedia/>;
   if(slug==='aji'&&pathname==='/fish/aji')return <AjiMedia/>;
   if(slug==='madai'&&pathname==='/fish/madai')return <MadaiMedia/>;
+  if(slug==='kasago'&&pathname==='/fish/kasago')return <KasagoMedia/>;
 
   return <div className={`${s.wrap} ${className}`}>
     {!failed?<img src={src} alt={`${name}の図鑑画像`} width={slug==='saba'?1891:undefined} height={slug==='saba'?831:undefined} onError={()=>setFailed(true)}/>:<FishArt slug={slug} label={name}/>} 
